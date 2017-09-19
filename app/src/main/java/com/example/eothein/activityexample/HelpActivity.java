34f3c87ad582e30ce6d4267f2f24cd8eb35568ec
eothein <jens.buysse@hogent.be>
2017-09-19 10:49:06 +0200

    Add new HelpAcitivty
    The HelpActivity is added to the project and a OnClickListener is added to the button.

---
+++ app/src/main/java/com/example/eothein/activityexample/HelpActivity.java
@@ -0,0 +1,19 @@
+package com.example.eothein.activityexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+/**
+ * @author uthor Jens Buysse
+ * @date 19/09/2017
+ * @version ersion 0.1.0
+ */
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
--- app/src/main/java/com/example/eothein/activityexample/MainActivity.java
+++ app/src/main/java/com/example/eothein/activityexample/MainActivity.java
@@ -1,7 +1,19 @@
 package com.example.eothein.activityexample;
 
+/**
+ * @author Jens Buysse
+ * @version 0.1.0
+ * @date 19/09/2017
+ *
+ * This is an example activity, demonstrating some of the features of the Activity and the
+ * Activity life cycle.
+ */
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +21,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Create an explicit intent which directs to the new Activity
+                Intent intent = new Intent(MainActivity.this, HelpActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
